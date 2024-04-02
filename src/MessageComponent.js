800ad74a483edd3dde25b4d9bc9eace32633ecee
Biljana  Nedic <biljana.nedic@brezasoftware.com>
2024-04-02 12:34:53 +0200

    prvi commit

---
+++ src/MessageComponent.js
@@ -0,0 +1,32 @@
+// Import React and the message function from message.js
+import React from 'react';
+import message from './message';
+
+// Define a functional component to display the message
+const MessageComponent = () => {
+  // Call the message function
+  const result = message();
+  const name = result.split(' ')[0];
+  const age = result.split(' ')[2];
+
+  const paragraphStyle = {
+    fontSize: '24px',
+    fontWeight: 'bold',
+    color: 'blue'
+    // Add any other styles you want here
+  };
+
+  // Return UI elements displaying the message
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <p><span style={paragraphStyle}>{name}</span><span> you are {age} years old.</span></p>
+      <p> </p>
+      
+      
+    </div>
+  );
+};
+
+// Export the MessageComponent
+export default MessageComponent;
